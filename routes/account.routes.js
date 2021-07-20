ecc6fa83c6522517b1f120ebfb0dedb6c34c0a83
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-20 12:08:10 -0300

    account create route

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -5,3 +5,26 @@ const AccountModel = require("../models/Account.model");
 
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const attachCurrentUser = require("../middlewares/attachCurrentUser");
+
+router.post(
+  "/account",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res, next) => {
+    try {
+      const { type } = req.body;
+      const loggedInUser = req.currentUser;
+
+      const lastInsertedAccount = AccountModel.find(
+        {},
+        { accountNumber: 1, _id: 0 }
+      )
+        .sort({ accountNumber: -1 })
+        .limit(1);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+module.exports = router;
